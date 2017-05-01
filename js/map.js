28ae42800d045bdd7f1108cc0a0b3f68112ce657
agrygo <andreagrygo@gmail.com>
2017-05-01 11:57:57 -0600

    first commit

---
+++ js/map.js
@@ -0,0 +1,9 @@
+var map;
+
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 39.550538 , lng: -107.324776},
+        zoom: 8
+    });
+    
+}
