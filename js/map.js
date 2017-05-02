3a7ce26dd5af536d4aaaaff45a14390849c3597e
agrygo <andreagrygo@gmail.com>
2017-05-02 13:57:29 -0600

    resolve GeoJSON issue to plot ArcGIS data as GeoJSON

--- js/map.js
+++ js/map.js
@@ -1,9 +1,28 @@
 var map;
+/*var arcgis = 'CountyBuildings.json';
+var FeatureCollection = {
+  type: "FeatureCollection",
+  features: []
+}
 
+for (var i = 0; i < arcgis.features.length; i++) {
+  var feature = Terraformer.ArcGIS.parse(arcgis.features[i]);
+  feature.id = i;
+  FeatureCollection.features.push(feature)
+};
+*/
 function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.550538 , lng: -107.324776},
-        zoom: 8
+        center: {lat: 39.563513 , lng: -107.546957},  //approx center in New Castle
+        zoom: 10
     });
+
+    //used converted esri JSON file 
+    map.data.loadGeoJson('../data/CountyBuildings.geojson');
+    /*map.data.setStyle({
+        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"    
+        
+    });*/
+
     
 }
