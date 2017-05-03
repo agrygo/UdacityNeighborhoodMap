3b698f6db4bf659f714a922ad1ea61d3108fffda
agrygo <andreagrygo@gmail.com>
2017-05-03 13:09:19 -0600

    Add markers via GeoJSON.  Add infoWindows

--- js/map.js
+++ js/map.js
@@ -17,12 +17,33 @@ function initMap(){
         zoom: 10
     });
 
-    //used converted esri JSON file 
-    map.data.loadGeoJson('../data/CountyBuildings.geojson');
+    //used converted esri JSON file to display locations
+    map.data.loadGeoJson('CountyBuildings.geojson');
     /*map.data.setStyle({
-        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"    
+        fillColor: 'blue'
         
     });*/
+
+    var features = $.getJSON('CountyBuildings.geojson');
+    console.log(features);
+
+    //create infoWindow 
+    infoWindow = new google.maps.InfoWindow ({
+        content: ""
+    }); 
+
+    //click event for markers and create infoWindow data
+    map.data.addListener('click', function(event){
+        name = event.feature.f.NAME;
+        address = event.feature.f.ADDRESS;
+        phone = event.feature.f.PHONE;
+        content = "<b>" + name + "</b><br>" + address + "<br>" + "<a href='tel:+" + phone + "'>" + phone + "</a>"
+        console.log(content);
+        infoWindow.setContent(content);
+        var anchor = new google.maps.MVCObject();
+        anchor.set("position", event.latLng);
+        infoWindow.open(map, anchor);
+    })
 
     
 }
